4ab7c104abe3557d420994d10306f66a360b310f
stankop <stanko.polic32@gmail.com>
2023-04-12 15:33:54 +0200

    Performanse

--- frontend/src/compontents/UI/categories.jsx
+++ frontend/src/compontents/UI/categories.jsx
@@ -1,98 +1,98 @@
 export const catMemory = [
     {
-      _id: 34,
-      name: 'Sedumi i čuvarkuće',
-      description: 'Sedumi i cuvarkuce',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/SEDUMI_I_%C4%8CUVARKU%C4%86E_NASLOVNA_28KW1JW.webp',
-      productNumber: '4',
-      order: 6
+      _id: 31,
+      name: 'Ukrasno žbunje',
+      description: 'Ukrasno žbunje odnosno šiblje su drvenaste biljke koje, uglavnom, nemaju centralno stablo. Mogu biti zimzeleni i listopadni, u različitim veličinama, oblicima i bojama. Od nekih vrsta možemo formirati atraktivnu živu ogradu ili ih kombinovati sa drugim biljnim vrstama kao što su tuje i trajnice.  Neki se čak adekvatnim orezivanjem mogu formirati u oblik drveta. Ukrasno žbunje mogu krasiti cvetovi, dok su kod drugih glavni adut njihov dekorativni list. Svakako su nezaobilazan deo vrta.',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/UKRASNO_ZBUNJE_NASLOVNA.jpg',
+      productNumber: '19',
+      order: 3
     },
     {
       _id: 41,
-      name: 'Začinsko i aromatično bilje',
-      description: 'Zacinsko i aromaticno bilje je ovo descriptivno.',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/ZA%C4%8CINSKO_BILJE_NASLOVNA_ez0usbd.jpg',
+      name: 'Proizvodi na akciji',
+      description: '',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/akcijaK.jpg',
       productNumber: '0',
-      order: 7
+      order: 12
+    },
+    {
+      _id: 29,
+      name: 'Penjačice i Puzavice',
+      description: 'Penjačice-puzavice mogu biti listopadne ili zimzelene biljke, koje se brzo penju i prekrivaju zidove, ograde, pergole, kuće ili zemljište. Postoje puzavice koje se same penju uz podlogu i pričvršćuju uz nju kao npr bršljan, Devojačka lozica i puzavice koje zahtevaju potporu i konstrukciju za penjanje poput Visterije i Orlovih noktiju.Ponekad se mogu kombinovati i više puzavica zajedno, naročito je dobro kada su to biljke koje se upotpunjuju cvetajući u različitim periodima.Puzavice se sve više traže, jer brzo rastu i za kratak period mogu da prekriju ili zaklone neke površine koje su neugledne poput starih šupa ili ruševnih zidova.',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/PUZAVICE_NASLOVNA_H0GDnG5_29mqk6D.jpg',
+      productNumber: '6',
+      order: 4
     },
     {
       _id: 35,
       name: 'Lišćari',
-      description: 'Liscari',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/LI%C5%A0%C4%86ARI_NASLOVNA_CVF93O4.jpg',
-      productNumber: '1',
+      description: 'Lišćari su dugovečne drvenaste biljke koje u našim uslovim rastu od 1m do 40m visine.\r\nZa našu planetu su od posebnog značaja jer predstavljaju pluća Zemlje, sprečavaju eroziju i održavaju ekosisteme. Lišćarske listopadne šume rastu u umereno kontinentalnom pojasu kome pripada i Srbija. Zbog svoje raznolikosti i estetske privlačnosti kolorita lišća, cvetova, oblika krošnja nezaobilazi su deo pejzažne arhitekture.\r\n  \r\n<br />\r\nAko razmišljate godinu dana unapred, posejte seme. \r\n<br/>\r\nAko razmišljate deset godina unapred, posadite drvo.',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/LI%C5%A0%C4%86ARI_NASLOVNA_QrWVYbE.jpg',
+      productNumber: '11',
       order: 9
+    },
+    {
+      _id: 33,
+      name: 'Ukrasne trave',
+      description: 'Ukrasne trave spadaju u veoma efektne biljke, koje se brzo šire a njihovo cvetanje krajem leta je posebno interesantno. Spadaju u veoma otporne biljke, ne traže posebne zahteve zemljišta. Mogu da rastu od 20cm pa sve do 2-3m. Postoji čitav dijapazon boja u kojima možemo naći ukrasne trave - od  zelene, plave, braon, zlatne, crvene, prugaste do crne. Vrlo često se kombinuju sa lišćarskim i četinarskim drvećem. Na proleće, kada prođe rizik od ranih mrazeva, poželjno je orezati ih na 10-15 cm iznad zemlje.',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/UKRASNE_TRAVE_NASLOVNA_BmH4P82.jpg',
+      productNumber: '10',
+      order: 5
+    },
+    {
+      _id: 32,
+      name: 'Začinsko i aromatično bilje',
+      description: 'Začinsko i aromatično bilje vrlo je pogodno za gajenje po principima organskog uzgoja iz nekoliko razloga – lako se gaji, lepo izgleda i u mnogim slučajevima nema problema sa napadom štetočina i bolesti.\r\nPored toga, gajenje začinskog i lekovitog bilja povećava raznovrsnost bašte, a mnoge vrste privlače korisne insekte, koji dolaze na šarenolike cvetove da uživaju u polenu i nektaru. \r\nTakođe od njih možete napraviti poseban deo u Vašem vrtu kao Aromatičnu baštu, koju možete koristiti cele godine za sveže začine, za čajeve i tinkture ili ih gajiti u posudama na terasama i balkonima za one koji nemaju dvorišta.',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/ZA%C4%8CINSKO_BILJE_NASLOVNA_ok4HEDW.jpg',
+      productNumber: '9',
+      order: 7
     },
     {
       _id: 40,
       name: 'Sadnice voća',
-      description: 'Sadnice voca',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/SADNICE_VOCA_NASLOVNA_5Hsbvvr.webp',
-      productNumber: '0',
+      description: '',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/SADNICE_VOCA_NASLOVNA.jpg',
+      productNumber: '1',
       order: 10
     },
     {
       _id: 39,
       name: 'Ostalo',
-      description: 'Ostalo',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/OSTALO_NASLOVNA_meQvgLk.webp',
-      productNumber: '0',
+      description: '',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/OSTALO_NASLOVNA.jpg',
+      productNumber: '7',
       order: 11
     },
     {
-      _id: 43,
-      name: 'Proizvodi na akciji',
-      description: 'Proixzvodi super na akciji i popustu.',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/magnolia-black-tulip2-1-350x350_h7qXmvz.jpg',
-      productNumber: '1',
-      order: 12
+      _id: 37,
+      name: 'Biljke za hlad',
+      description: 'Prazni zemljani krugovi ispod krošnji drveća umeju da naruže celokupan izgled dvorišta. Prostor u senci stabala može da izgleda zeleno i šareno, uz pravi odabir cveća i biljaka. Od fantastičnih hosti, hojhera i astilbi, preko bršljanova do prelepih zimzelenih grmova poput skimije i aukube moguće je na mnogo načina urediti kutak u senovitom delu bašte. Klupicu u sred baštice u hladu i leto može da počne.',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/biljke_za_senku_naslovna.jpg',
+      productNumber: '7',
+      order: 2
+    },
+    {
+      _id: 34,
+      name: 'Sedumi i čuvarkuće',
+      description: 'Sedumi su grupa sukulenata sa karakterističnim zadebljalim listovima koji služe kao rezerva vode što ovim biljkama omogućuje opstanak u veoma toplim i suvim staništima sa retkim padavinama. \r\nZbog otpornosti na sušu ali i na veoma niske temperature zimi, sedumi su se nametnuli kao idealne biljke za formiranje zelenih – živih krovova. \r\nZa razliku od svih drugih biljaka kojima je potrebno obilno zalivanje, sedumima je potreba za vodom minimalna što ih kandiduje za idealne u pogledu smanjenja njene potrošnje. \r\n\r\nNa svetu postoji više od 500 vrsta seduma, dok je kod nas prisutno više desetina vrsta različitih boja stabla, lista i cveta, kao i različitih tekstura, što ih takođe čini pogodnim za razna kombinovanja i formiranje oblika, logotipa ili šara. \r\nPored primene na zelenim krovovima, sedumi nalaze primenu i na zelenim zidovima i kamenim baštama. \r\n\r\nČuvarkuća, biljka poznata od davnina i u narodu mnogo korišćena. Njen latinski naziv Sempervivum znači Uvek živa. Još stari Grci i Rimljani su je sadili pored drugih biljaka smatrajući da će ih čuvarkuća štititi od raznih insekata i štetočina. To je višegodišnja biljka koja raste od 10 do 60 cm. Listovi čuvarkuće su složeni, sočni i debeli. Zahvalna je za gajenje, jer ne traži zalivanje ukoliko se gaji u dvorišnim uslovima. Gajenje čuvarkuće u kućnim uslovima podrazumeva vrlo malo zalivanja, tako da mnogi upoređuju gajenje ove biljke sa gajenjem kaktusa.Čuvarkuća cveta u periodu od oktobra do novembra tako da se tada sakupljaju njeni listovi kako za čajeve tako i za druge preparate.',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/SEDUMI_I_%C4%8CUVARKU%C4%86E_NASLOVNA.jpg',
+      productNumber: '17',
+      order: 6
     },
     {
       _id: 28,
       name: 'Trajnice-perene',
-      description: 'Višegodišnje biljke, nazvane perene, po latinskom perennis, što znači trajan. Žive od 3 – 30 godina. Perene se razlikuju po visini, po veličini i obliku lista,\r\n po veličini i boji cveta i po vremenu cvetanja. Visina im se kreće od par centimetara do preko 2 m. Cvetovi svih boja sunčevog spektra, uključujući i belu, a vreme cvetanja, zavisno od vrste, od snega do snega.\r\n Po precvetavanju perene orezati, ne dozvoliti stvaranje plodova (sem dekorativnih), koji crpe biljku. Da bi na jednoj zelenoj ili cvetnoj površini perene došle do izražaja, sade se, zavisno od veličine, pojedinačno\r\n ili u grupama. Visoke, markantne perene sade se posebno na travnjaku, ili u perenjaku iza ostalih perena, dok se niske sade po više zajedno u prednjem planu.\r\n Redovne mere nege, uz malo ljubavi, doprineće da se divite svojim perenama i da svako jutro isčekujete koji će se novi cvet otvoriti. \r\n Jednom zasnovan perenjak tokom godina će, verovatno, pretrpeti izvesne korekcije, a to će svake godine davati novu, uvek lepšu sliku. \r\n S obzirom na izuzetno veliku raznolikost, postoji neograničena mogućnost kombinovanja samih perena, kao i kombinovanja sa niskim četinarima ili cvetnim šibljem.\r\n\r\n <br />\r\n <br />\r\n <strong>Prednosti trajnica:</strong>  \r\n    <Ul>  \r\n        <li>životni vek im je u proseku iznad 5 godina</li>\r\n        <li>brzo napreduju i brzo se šire</li>        \r\n        <li>skromnih su zahteva i brzo se prilogođavaju sredini u kojoj rastu</li>        \r\n        <li>za svaku različitu sredinu postoji odgovarajuća trajnica</li>       \r\n        <li>nezamisliv broj tekstura listova i boja cveta</li>       \r\n        <li>pod uticajem različitih godišnjih doba uvek su različite i zanimljive</li>        \r\n        <li>troškovi održavanja su mali</li>\r\n    </ul>',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/perene_naslovna5_Mv3aZRi.jpeg',
-      productNumber: '0',
+      description: 'Perene odnosno trajnice su dobile naziv jer im alatinski naziv Perennis znači trajan. Životni vek ovih biljaka je od 3 do 30 godina.\r\nTrajnice – perene mogu biti zimzelene i listopadne. Kod listopadnih perena svake zime izmrzne nadzemni deo, a podzemni deo prezimljava i naredne godine se formira nova biljka koja će biti šira i veća nego prethodne godine. \r\nPerene se razlikuju i po visini, po veličini i obliku lista, po veličini i boji cveta i po vremenu cvetanja. Visina im se kreće od par centimetara do preko 2 m.  Po precvetavanju orezati cvetne drške, radi produžavanja perioda i bujnosti cvetanja. Da bi na jednoj zelenoj ili cvetnoj površini perene došle do izražaja, sade se, zavisno od veličine, pojedinačno ili u grupama. Visoke, markantne perene sade se posebno na travnjaku, ili u perenjaku iza ostalih perena, dok se niske sade po više zajedno u prednjem planu. Redovne mere nege, uz malo ljubavi, doprineće da se divite svojim perenama i da svako jutro isčekujete koji će se novi cvet otvoriti. Jednom zasnovan perenjak tokom godina će, verovatno, pretrpeti izvesne korekcije, a to će svake godine davati novu, uvek lepšu sliku. S obzirom na izuzetno veliku raznolikost, postoji neograničena mogućnost kombinovanja samih perena, kao i kombinovanja sa niskim četinarima ili ukrasnim šibljem.',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/perene_naslovna5.jpg',
+      productNumber: '64',
       order: 1
     },
     {
       _id: 36,
       name: 'Četinari',
-      description: 'Četinari su vrsta, najčešće, zimzelenog drveća koje se danas u sve većoj meri upotrebljava u pejzažnoj arhitekturi za ozelenjavanje i oblikovanje zelenih površina. Četinari predstavljaju omiljenu i najčešće korišćenu kategoriju sadnog materijala u savremenim rtovima. Jedan od razloga je i što su četinari uglavnom zimzeleni i otporni na mraz pa svoju dekorativnost ne gube ni tokom zime kada je u periodu mirovanja vegetacije,većina biljaka ostala bez lišća. Drugi razlog jeste to što odlično podnose orezivanje, pa se od njih mogu praviti najrazličitiji oblici. Počev od običnih rezanih živih ograda, pa do najsloženijih šišanih (topijarnih) formi. Osim gore navedenih prednosti,četinari su i veoma varijabilna vrsta. Tako da danas za dizajniranje svojih dvorišta ili vrtova možete koristiti veliki broj sadnica različitih formi, boja i oblika koji su pogodni za međusobno kombinovanje, kao i za kombinovanje sa drugim biljnim vrstama.',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/berberis-erecta-1-350x350_x2lgCSg.jpg',
-      productNumber: '59',
+      description: 'Četinari su vrsta, najčešće, zimzelenog drveća koje se danas u sve većoj meri upotrebljava u pejzažnoj arhitekturi za ozelenjavanje i oblikovanje zelenih površina. \r\nČetinari predstavljaju omiljenu i najčešće korišćenu kategoriju sadnog materijala u savremenim rtovima. Jedan od razloga je i što su četinari uglavnom zimzeleni i otporni na mraz pa svoju dekorativnost ne gube ni tokom zime kada je u periodu mirovanja vegetacije,većina biljaka ostala bez lišća. Drugi razlog jeste to što odlično podnose orezivanje, pa se od njih mogu praviti najrazličitiji oblici. Počev od običnih rezanih živih ograda, pa do najsloženijih šišanih (topijarnih) formi. Osim gore navedenih prednosti,četinari su i veoma varijabilna vrsta. Tako da danas za dizajniranje svojih dvorišta ili vrtova možete koristiti veliki broj sadnica različitih formi, boja i oblika koji su pogodni za međusobno kombinovanje, kao i za kombinovanje sa drugim biljnim vrstama.',
+      image: 'https://policplantblob.blob.core.windows.net/policplant-container/naslovna_za_kategoriju_%C4%8Detinari.jpg',
+      productNumber: '12',
       order: 8
-    },
-    {
-      _id: 42,
-      name: 'Biljke za hlad',
-      description: 'Biljke za hladic i za medic.',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/biljke_za_senku_naslovna_UUTSeAN.webp',
-      productNumber: '0',
-      order: 2
-    },
-    {
-      _id: 31,
-      name: 'Ukrasno žbunje',
-      description: 'Ukrasno zbunje',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/UKRASNO_ZBUNJE_NASLOVNA_i25H78P.JPG',
-      productNumber: '0',
-      order: 3
-    },
-    {
-      _id: 30,
-      name: 'Penjacice i puzavice',
-      description: 'Puzavice',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/PUZAVICE_NASLOVNA_vya8BlX.jpg',
-      productNumber: '0',
-      order: 4
-    },
-    {
-      _id: 33,
-      name: 'Ukrasne trave',
-      description: 'Ukrasne trave',
-      image: 'https://policplantblob.blob.core.windows.net/policplant-container/UKRASNE_TRAVE_NASLOVNA_JJniaOT.jpg',
-      productNumber: '0',
-      order: 5
     }
   ]
